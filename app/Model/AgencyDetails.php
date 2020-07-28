a92bc17ac529131c5bb79004de2f55fc206d44c8
webexecute <webexecuteinfotech@gmail.com>
2020-07-28 23:42:49 +0530

    change in agent

--- app/Model/AgencyDetails.php
+++ app/Model/AgencyDetails.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class AgencyDetails extends Model
 {
     //
+    protected $table ="agencydetails";
 }
---
+++ app/Model/AgentDetails.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AgentDetails extends Model
+{
+    protected $table ="agentdetails";
+
+    public function getAgentDetails($id){
+
+    }
+}
