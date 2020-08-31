19caa8b86373f0b27297acb5ef1d0c1d87ffca28
kartikdesai <kartikdesai123@gmail.com>
2020-08-31 23:10:43 +0530

    chnage as per require

--- resources/views/frontend/pages/profile/addmyagent.blade.php
+++ resources/views/frontend/pages/profile/addmyagent.blade.php
@@ -72,28 +72,4 @@ My profile -->
   <!--=================================
   My profile -->
 
-  <!--=================================
-  newsletter -->
-  <section class="py-5 bg-primary">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-5">
-          <h3 class="text-white mb-0">Sign up to our newsletter to get the latest news and offers.</h3>
-        </div>
-        <div class="col-md-7 mt-3 mt-md-0">
-          <div class="newsletter">
-            <form>
-              <div class="form-group mb-0">
-                <input type="email" class="form-control" placeholder="Enter email">
-              </div>
-              <button type="submit" class="btn btn-dark b-radius-left-none">Get notified</button>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-  <!--=================================
-  newsletter -->
- 
 @endsection
